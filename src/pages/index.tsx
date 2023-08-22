1c0b3a91bd23b392afce183ecdf8aeb69182b28c
Johannes Welsch <jwelsch@piazzablu.com>
2023-08-22 10:51:25 +0200

    1st

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,182 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { useState } from "react";
+import { useKeenSlider } from "keen-slider/react";
+import cn from "classnames";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const colors = ["#ecee15", "#f03636", "#36cbf0", "#DD71b0", "#22DDb9"];
+const sizes = [0, 1, 2, 3, 4];
 
 export default function Home() {
+  const [sliderRef, instanceRef] = useKeenSlider({
+    drag: false,
+    loop: false,
+    initial: 0,
+    slides: {
+      origin: "center",
+      perView: 1,
+      spacing: 0,
+    },
+    defaultAnimation: { duration: 600 },
+    vertical: true,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
+  });
+
+  const [selectedColorIndex, setSelectedColorIndex] = useState(0);
+  const [selectedSizeIndex, setSelectedSizeIndex] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <div ref={sliderRef} className="keen-slider h-screen relative">
+        {/* COLORS */}
+        <section className="relative keen-slider__slide section section-1 bg-blue-500">
+          <div className="flex justify-center items-center flex-col h-full">
+            <h2 className="text-white text-5xl font-neoSansProRegular mb-10">
+              Farbe wählen
+            </h2>
+            <div className="flex gap-4 mb-20">
+              {colors.map((color, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={cn(
+                      "cursor-pointer rounded-xl h-20 w-20 shadow-2xl",
+                      selectedColorIndex === index
+                        ? "border-8 border-white "
+                        : ""
+                    )}
+                    style={{ backgroundColor: colors[index] }}
+                    onClick={(e) => {
+                      setSelectedColorIndex(index);
+                    }}
+                  />
+                );
+              })}
+            </div>
+            {loaded && instanceRef.current && (
+              <button
+                className="z-10 cursor-pointer px-10 py-5 bg-green-500 text-white font-bold text-4xl rounded-xl shadow-2xl"
+                onClick={(e: any) => {
+                  console.log("klacked");
+                  instanceRef.current?.next();
+                }}
+                disabled={
+                  currentSlide ===
+                  instanceRef.current.track.details.slides.length - 1
+                }
+              >
+                WEITER &darr;
+              </button>
+            )}
+          </div>
+        </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        {/* SIZES */}
+        <section className="keen-slider__slide section section-1 bg-red-600">
+          <div className="flex flex-col justify-center items-center h-full">
+            <h2 className="text-white text-5xl font-neoSansProRegular mb-10">
+              Größe wählen
+            </h2>
+            <div className="flex gap-4 mb-20 items-baseline">
+              {sizes.map((color, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={cn(
+                      "cursor-pointer rounded-xl h-20 w-20 shadow-2xl",
+                      selectedSizeIndex === index
+                        ? "border-8 border-white "
+                        : ""
+                    )}
+                    onClick={() => setSelectedSizeIndex(index)}
+                    style={{
+                      width: 5 + index + "rem",
+                      height: 5 + index + "rem",
+                      backgroundColor: colors[selectedColorIndex],
+                    }}
+                  />
+                );
+              })}
+            </div>
+            {loaded && instanceRef.current && (
+              <button
+                className="z-10 cursor-pointer px-10 py-5 bg-green-500 text-white font-bold text-4xl rounded-xl shadow-2xl"
+                onClick={(e: any) => {
+                  console.log("klacked");
+                  instanceRef.current?.next();
+                }}
+                disabled={
+                  currentSlide ===
+                  instanceRef.current.track.details.slides.length - 1
+                }
+              >
+                WEITER &darr;
+              </button>
+            )}
+            {loaded && instanceRef.current && (
+              <button
+                className="absolute top-36 left-20 z-10 cursor-pointer px-10 py-5 text-white font-bold text-4xl rounded-xl shadow-2xl border-2"
+                onClick={(e: any) => {
+                  console.log("klacked");
+                  instanceRef.current?.prev();
+                }}
+              >
+                ZURÜCK &uarr;
+              </button>
+            )}
+          </div>
+        </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {/* 3 */}
+        <section className="keen-slider__slide section section-1 bg-green-600">
+          <div className="flex flex-col justify-center items-center h-full">
+            <h2 className="text-white text-5xl font-neoSansProRegular mb-10">
+              Größe wählen
+            </h2>
+            <div className="flex gap-4 mb-20 items-baseline">
+              {sizes.map((color, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={cn(
+                      "cursor-pointer rounded-xl h-20 w-20 shadow-2xl",
+                      selectedSizeIndex === index
+                        ? "border-8 border-white "
+                        : ""
+                    )}
+                    onClick={() => setSelectedSizeIndex(index)}
+                    style={{
+                      width: 5 + index + "rem",
+                      height: 5 + index + "rem",
+                      backgroundColor: colors[selectedColorIndex],
+                    }}
+                  />
+                );
+              })}
+            </div>
+            {loaded && instanceRef.current && (
+              <button
+                className="absolute top-36 left-20 z-10 cursor-pointer px-10 py-5 text-white font-bold text-4xl rounded-xl shadow-2xl border-2"
+                onClick={(e: any) => {
+                  console.log("klacked");
+                  instanceRef.current?.prev();
+                }}
+              >
+                ZURÜCK &uarr;
+              </button>
+            )}
+          </div>
+        </section>
       </div>
     </main>
-  )
+  );
 }
